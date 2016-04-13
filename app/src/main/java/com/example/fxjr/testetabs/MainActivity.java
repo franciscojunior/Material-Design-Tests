832dfe80b919f296b8e258d5da82eda86bc42440
Francisco Figueiredo Jr <francisco@npgsql.org>
2016-04-13 12:34:57 -0300

    Add back button support

--- app/src/main/java/com/example/fxjr/testetabs/MainActivity.java
+++ app/src/main/java/com/example/fxjr/testetabs/MainActivity.java
@@ -227,6 +227,8 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
+        } else if (searchShown) {
+            toggleSearchView();
         } else {
             super.onBackPressed();
         }
