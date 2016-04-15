1abd4598bc5b602ac8ee93c024ab3859377986d8
Francisco Figueiredo Jr <francisco@npgsql.org>
2016-04-14 23:35:25 -0300

    Implement hambuger icon animation, fab improvements
    Hanbuger icon is now animated between hamburger and arrow icons when
    doing a search through fab icon.
    
    fab icon now animates toolbar back if it is hidden because of scrolling.

--- app/src/main/java/com/example/fxjr/testetabs/MainActivity.java
+++ app/src/main/java/com/example/fxjr/testetabs/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.fxjr.testetabs;
 
 import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
@@ -9,6 +10,7 @@ import android.database.sqlite.SQLiteBlobTooBigException;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.BaseColumns;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -22,14 +24,18 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.drawable.DrawerArrowDrawable;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.support.v7.widget.AppCompatAutoCompleteTextView;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.AutoCompleteTextView;
 import android.widget.MultiAutoCompleteTextView;
 
@@ -49,14 +55,19 @@ public class MainActivity extends AppCompatActivity
     private List<FragmentFilterable> fragmentsToFilter = new ArrayList<>();
     private SimpleCursorAdapter mAdapter;
 
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private DrawerArrowDrawable drawerArrowDrawable;
 
+    private MultiAutoCompleteTextView search_cards;
 
     private static final String[] SUGGESTIONS = {
             "Bauru", "Sao Paulo", "Rio de Janeiro",
             "Bahia", "Mato Grosso", "Minas Gerais",
             "Tocantins", "Rio Grande do Sul"
     };
-    private MultiAutoCompleteTextView search_cards;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,7 +78,7 @@ public class MainActivity extends AppCompatActivity
 
 
         setContentView(R.layout.activity_main);
-        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         viewPager = (ViewPager) findViewById(R.id.viewpager);
@@ -75,6 +86,7 @@ public class MainActivity extends AppCompatActivity
         tabLayout = (TabLayout) findViewById(R.id.tablayout);
 
 
+        final AppBarLayout appbar = (AppBarLayout) findViewById(R.id.appbar);
 
         setupViewPager(viewPager);
         tabLayout.setupWithViewPager(viewPager);
@@ -82,14 +94,13 @@ public class MainActivity extends AppCompatActivity
 
         search_cards = (MultiAutoCompleteTextView) getLayoutInflater().inflate(R.layout.search_cards_multiautotext, null);
 
+        setupSearchTextView(search_cards);
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
 
                 // Reference: ﻿https://www.raywenderlich.com/103367/material-design
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ) {
@@ -106,26 +117,45 @@ public class MainActivity extends AppCompatActivity
 //            AutoCompleteTextView e = new AutoCompleteTextView(MainActivity.this);
 
 
-                if (!search_cards.isShown()) {
+                // Reference: http://stackoverflow.com/questions/11710042/expand-and-give-focus-to-searchview-automatically
+                // Had to add collapseActionView flag
+                //MenuItemCompat.expandActionView(searchMenuItem);
 
-                    getSupportActionBar().setDisplayShowTitleEnabled(false);
-                    toolbar.addView(search_cards);
+                // Show appbar so user can use the searchview.
+                // Reference: http://stackoverflow.com/questions/33958878/hide-show-toolbar-programmatically-on-coordinatorlayout
 
-                    search_cards.requestFocus();
-                }
-                else {
-                    toolbar.removeView(search_cards);
-                    getSupportActionBar().setDisplayShowTitleEnabled(true);
-                }
+                appbar.setExpanded(true);
+
+                toggleSearchView();
+
+//                if (!search_cards.isShown()) {
+//
+//                    getSupportActionBar().setDisplayShowTitleEnabled(false);
+//
+//                    toolbar.addView(search_cards);
+//
+//                    search_cards.requestFocus();
+//                }
+//                else {
+//                    toolbar.removeView(search_cards);
+//                    getSupportActionBar().setDisplayShowTitleEnabled(true);
+//
+//                }
 
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+
+        // Reference: http://stackoverflow.com/questions/30824324/clicking-hamburger-icon-on-toolbar-does-not-open-navigation-drawer?lq=1
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawer.setDrawerListener(toggle);
+//        toggle.syncState();
+
+        drawerArrowDrawable = new DrawerArrowDrawable(getSupportActionBar().getThemedContext());
+        toolbar.setNavigationIcon(drawerArrowDrawable);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -136,7 +166,90 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-    private void setupSearchView(final SearchView searchView) {
+
+    private void toggleSearchView() {
+        playDrawerToggleAnim((DrawerArrowDrawable)toolbar.getNavigationIcon());
+
+        if (search_cards.isShown()) {
+            toolbar.removeView(search_cards);
+            getSupportActionBar().setDisplayShowTitleEnabled(true);
+
+        } else {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+            toolbar.addView(search_cards);
+
+            search_cards.requestFocus();
+
+        }
+
+    }
+
+    // Reference: http://stackoverflow.com/questions/26835209/appcompat-v7-toolbar-up-back-arrow-not-working
+
+    public static void playDrawerToggleAnim(final DrawerArrowDrawable d) {
+        float start = d.getProgress();
+        float end = Math.abs(start - 1);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            ValueAnimator offsetAnimator = ValueAnimator.ofFloat(start, end);
+            offsetAnimator.setDuration(300);
+            offsetAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+            offsetAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    float offset = 0;
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                        offset = (Float) animation.getAnimatedValue();
+                    }
+                    d.setProgress(offset);
+                }
+            });
+            offsetAnimator.start();
+        }
+        else
+            d.setProgress(end);
+    }
+
+    private void setupSearchTextView(final MultiAutoCompleteTextView searchView) {
+
+
+        searchView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                Log.d(TAG, "onQueryTextChange... ");
+
+                String searchConstraint = "%" + s.toString().toLowerCase() + "%";
+
+
+
+//                ((CardsListFragment)((ViewPagerAdapter)viewPager.getAdapter()).getCachedItem(0)).setFilter(" and lower(name) like ?", new String[] {newText});
+//                ((CardsListFragment)((ViewPagerAdapter)viewPager.getAdapter()).getCachedItem(1)).setFilter(" and lower(name) like ?", new String[] {newText});
+
+
+                for (FragmentFilterable fragment:
+                        fragmentsToFilter) {
+
+                    fragment.setFilter(" and lower(name) like ?", new String[] {searchConstraint});
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+    }
+
+    private void setupSearchViewold(final SearchView searchView) {
 
         // Reference: http://stackoverflow.com/questions/23658567/android-actionbar-searchview-suggestions-with-a-simple-string-array
         final String[] from = new String[] {"cityName"};
@@ -221,6 +334,12 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
+        } else if (search_cards.isShown()) {
+            if (search_cards.getText().length() > 0) {
+                search_cards.setText("");
+            } else {
+                toggleSearchView();
+            }
         } else {
             super.onBackPressed();
         }
@@ -255,12 +374,24 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == android.R.id.home) {
+            if (search_cards.isShown()) {
+                if (search_cards.getText().length() > 0) {
+                    search_cards.setText("");
+                } else {
+                    toggleSearchView();
+                }
+
+            }
+            else {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
             return true;
-//        } else if (id == R.id.action_search) {
-//
-//
-//
+
+        } else if (id == R.id.action_settings) {  //noinspection SimplifiableIfStatement
+
+
+            return true;
         }
 
 
