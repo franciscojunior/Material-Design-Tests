5d919b9f9e76725240f828fef57dcabdfbb654f4
Francisco Figueiredo Jr <francisco@npgsql.org>
2016-05-25 11:52:40 -0300

    Add capacity filtering support

--- app/src/main/java/com/example/fxjr/testetabs/FilterModel2.java
+++ app/src/main/java/com/example/fxjr/testetabs/FilterModel2.java
@@ -1,17 +1,25 @@
 package com.example.fxjr.testetabs;
 
+import android.util.Log;
+
 /**
  * Created by FranciscoJunior on 20/05/2016.
  */
 public class FilterModel2 {
 
+    private static final String TAG = "FilterModel2";
+
     CharSequence name = "";
 
     final String GROUP_CRYPT_FILTER = "(_group = '*' or _group in (?))";
+    final String CAPACITY_CRYPT_FILTER = "Cast(capacity as integer) between ";
 
     boolean groups[] = new boolean[6];
     boolean groupsFilterChanged;
     String groupsFilterCached = "";
+
+    int capacityMin = 1;
+    int capacityMax = 11;
 
     public String getGroupsQuery() {
 
@@ -70,6 +78,7 @@ public class FilterModel2 {
 
         String groupsQuery = getGroupsQuery();
 
+        // Groups processing
         if (groupsQuery.length() > 0) {
             result.append(" and ");
             result.append(groupsQuery);
@@ -77,6 +86,13 @@ public class FilterModel2 {
         }
 
 
+        // Capacity processing
+
+        result.append(" and ");
+        result.append(CAPACITY_CRYPT_FILTER).append(capacityMin).append(" AND ").append(capacityMax);
+
+
+        Log.d(TAG, "getCryptFilterQuery() returned: " + result);
 
         return result.toString();
 
@@ -85,5 +101,14 @@ public class FilterModel2 {
 
     public String getLibraryFilterQuery() {
         return "";
+    }
+
+    public void setCapacityMin(int progress) {
+        capacityMin = progress;
+
+    }
+
+    public void setCapacityMax(int progress) {
+        capacityMax = progress;
     }
 }
