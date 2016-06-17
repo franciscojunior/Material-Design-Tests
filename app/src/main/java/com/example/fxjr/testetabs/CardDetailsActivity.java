27de67e6d250098c1bcc053d0a80d1932c41b85b
Francisco Figueiredo Jr <francisco@npgsql.org>
2016-06-17 17:18:47 -0300

    Add discipline images to card details.

--- app/src/main/java/com/example/fxjr/testetabs/CardDetailsActivity.java
+++ app/src/main/java/com/example/fxjr/testetabs/CardDetailsActivity.java
@@ -1,17 +1,28 @@
 package com.example.fxjr.testetabs;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.HashMap;
 
 public class CardDetailsActivity extends AppCompatActivity {
 
     private static final String TAG = "CardDetailsActivity";
+    private static String QUERY_CRYPT = "select Name, Type, Clan, Disciplines, CardText, Capacity, Artist, _Set, _Group from crypt where _id = ?";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +42,168 @@ public class CardDetailsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+        fillImageViewsDrawablesMap(this);
+
+
+        setupDisciplineImagesArray();
+
+        setupCardData();
+
+    }
+
+    private void setupDisciplineImagesArray() {
+
+        disciplineImageViews[0] = (ImageView) findViewById(R.id.img_card_details_discipline1);
+        disciplineImageViews[1] = (ImageView) findViewById(R.id.img_card_details_discipline2);
+        disciplineImageViews[2] = (ImageView) findViewById(R.id.img_card_details_discipline3);
+        disciplineImageViews[3] = (ImageView) findViewById(R.id.img_card_details_discipline4);
+        disciplineImageViews[4] = (ImageView) findViewById(R.id.img_card_details_discipline5);
+        disciplineImageViews[5] = (ImageView) findViewById(R.id.img_card_details_discipline6);
+
+
+    }
+
+
+    private void setupCardData() {
+
         TextView txtCardText = (TextView) findViewById(R.id.cardText);
 
 
-        String cardName = getIntent().getExtras().getString("cardName");
-        String cardText = getIntent().getExtras().getString("cardText");
+
+        long cardId = getIntent().getExtras().getLong("cardId");
+
+        String query;
+
+        query = QUERY_CRYPT;
+
+        SQLiteDatabase db = DatabaseHelper.getDatabase();
+        Cursor c = db.rawQuery(query, new String[] {String.valueOf(cardId)});
+        c.moveToFirst();
+
+        String cardName = c.getString(0);
+        String cardType = c.getString(1);
+        String cardClan = c.getString(2);
+        String cardDisciplines = c.getString(3);
+        String cardText = c.getString(4);
+        String cardCapacity = c.getString(5);
+        String cardArtist = c.getString(6);
+        String cardSetRarity = c.getString(7);
+        String cardGroup = c.getString(8);
+
+        c.close();
+
+
+
+
+        String[] disciplines = cardDisciplines.split(" ");
+
+        int disIndex = 0;
+        for (String discipline : disciplines) {
+            disciplineImageViews[disIndex].setImageDrawable(imageViewsDrawablesMap.get(discipline));
+            //viewHolder.disciplineImageViews[disIndex].setImageBitmap(imageViewsDrawablesMap.get(discipline));
+            disciplineImageViews[disIndex].setVisibility(View.VISIBLE);
+            disIndex++;
+        }
+
+        clearDisciplineImageViews(disIndex);
 
 
         getSupportActionBar().setTitle(cardName);
         txtCardText.setText(cardText);
+    }
+
+
+    private static final HashMap<String, Drawable> imageViewsDrawablesMap = new HashMap<>();
+
+
+    private static void fillImageViewsDrawablesMap(Context context) {
+
+
+
+        if (imageViewsDrawablesMap.isEmpty()) {
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 4;
+
+            Resources res = context.getResources();
+
+
+            imageViewsDrawablesMap.put("abo", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_abombwe, options)));
+            imageViewsDrawablesMap.put("ABO", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_abombwe_sup, options)));
+            imageViewsDrawablesMap.put("ani", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_animalism, options)));
+            imageViewsDrawablesMap.put("ANI", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_animalism_sup, options)));
+            imageViewsDrawablesMap.put("aus", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_auspex, options)));
+            imageViewsDrawablesMap.put("AUS", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_auspex_sup, options)));
+            imageViewsDrawablesMap.put("cel", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_celerity, options)));
+            imageViewsDrawablesMap.put("CEL", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_celerity_sup, options)));
+            imageViewsDrawablesMap.put("chi", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_chimerstry, options)));
+            imageViewsDrawablesMap.put("CHI", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_chimerstry_sup, options)));
+            imageViewsDrawablesMap.put("dai", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_daimoinon, options)));
+            imageViewsDrawablesMap.put("DAI", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_daimoinon_sup, options)));
+            imageViewsDrawablesMap.put("dem", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_dementation, options)));
+            imageViewsDrawablesMap.put("DEM", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_dementation_sup, options)));
+            imageViewsDrawablesMap.put("dom", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_dominate, options)));
+            imageViewsDrawablesMap.put("DOM", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_dominate_sup, options)));
+            imageViewsDrawablesMap.put("for", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_fortitude, options)));
+            imageViewsDrawablesMap.put("FOR", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_fortitude_sup, options)));
+            imageViewsDrawablesMap.put("mel", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_melpominee, options)));
+            imageViewsDrawablesMap.put("MEL", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_melpominee_sup, options)));
+            imageViewsDrawablesMap.put("myt", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_mytherceria, options)));
+            imageViewsDrawablesMap.put("MYT", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_mytherceria_sup, options)));
+            imageViewsDrawablesMap.put("nec", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_necromancy, options)));
+            imageViewsDrawablesMap.put("NEC", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_necromancy_sup, options)));
+            imageViewsDrawablesMap.put("obe", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_obeah, options)));
+            imageViewsDrawablesMap.put("OBE", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_obeah_sup, options)));
+            imageViewsDrawablesMap.put("obf", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_obfuscate, options)));
+            imageViewsDrawablesMap.put("OBF", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_obfuscate_sup, options)));
+            imageViewsDrawablesMap.put("obt", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_obtenebration, options)));
+            imageViewsDrawablesMap.put("OBT", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_obtenebration_sup, options)));
+            imageViewsDrawablesMap.put("pot", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_potence, options)));
+            imageViewsDrawablesMap.put("POT", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_potence_sup, options)));
+            imageViewsDrawablesMap.put("pre", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_presence, options)));
+            imageViewsDrawablesMap.put("PRE", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_presence_sup, options)));
+            imageViewsDrawablesMap.put("pro", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_protean, options)));
+            imageViewsDrawablesMap.put("PRO", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_protean_sup, options)));
+            imageViewsDrawablesMap.put("qui", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_quietus, options)));
+            imageViewsDrawablesMap.put("QUI", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_quietus_sup, options)));
+            imageViewsDrawablesMap.put("san", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_sanguinus, options)));
+            imageViewsDrawablesMap.put("SAN", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_sanguinus_sup, options)));
+            imageViewsDrawablesMap.put("ser", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_serpentis, options)));
+            imageViewsDrawablesMap.put("SER", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_serpentis_sup, options)));
+            imageViewsDrawablesMap.put("spi", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_spiritus, options)));
+            imageViewsDrawablesMap.put("SPI", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_spiritus_sup, options)));
+            imageViewsDrawablesMap.put("tem", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_temporis, options)));
+            imageViewsDrawablesMap.put("TEM", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_temporis_sup, options)));
+            imageViewsDrawablesMap.put("thn", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_thanatosis, options)));
+            imageViewsDrawablesMap.put("THN", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_thanatosis_sup, options)));
+            imageViewsDrawablesMap.put("tha", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_thaumaturgy, options)));
+            imageViewsDrawablesMap.put("THA", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_thaumaturgy_sup, options)));
+            imageViewsDrawablesMap.put("val", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_valeren, options)));
+            imageViewsDrawablesMap.put("VAL", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_valeren_sup, options)));
+            imageViewsDrawablesMap.put("vic", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_vicissitude, options)));
+            imageViewsDrawablesMap.put("VIC", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_vicissitude_sup, options)));
+            imageViewsDrawablesMap.put("vis", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_visceratika, options)));
+            imageViewsDrawablesMap.put("VIS", new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_dis_visceratika_sup, options)));
+        }
+    }
+
+    // Discipline images
+
+    private ImageView[] disciplineImageViews = new ImageView[6];
+
+    private void clearDisciplineImageViews() {
+        for (ImageView disciplineImageView : disciplineImageViews) {
+            disciplineImageView.setImageDrawable(null);
+        }
+
+    }
+
+    private void clearDisciplineImageViews(int fromIndex) {
+        // Optimization to clear imageviews only from fromIndex and above.
+        for (int i = fromIndex; i < disciplineImageViews.length; i++) {
+            disciplineImageViews[i].setVisibility(View.INVISIBLE);
+
+        }
 
     }
 }
